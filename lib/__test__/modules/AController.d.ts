f6ca6228e9fabcd4f4106b9a8bc660a8b213f468
javarouka <javarouka@gmail.com>
2019-03-03 16:18:52 +0900

    fix test

--- lib/__test__/modules/AController.d.ts
+++
@@ -1,6 +0,0 @@
-import BController from './BController';
-import HttpController from './HttpController';
-export default class AController implements HttpController {
-    constructor(bController: BController);
-    hello(): string;
-}
